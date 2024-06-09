fafc3190f9f979c3de3da617c298b4136ad108c3
Bright Misael Zega <brightmisaelzega@gmail.com>
2024-06-10 00:11:18 +0700

    Add files via upload

--- cypress/auth/src/app/config.js
+++ cypress/auth/src/app/config.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "###########",
-  authDomain: "###########",
-  projectId: "##########",
-  storageBucket: "#########",
-  messagingSenderId: "##############",
-  appId: "#########",
-  measurementId: "######"
+  apiKey: "AIzaSyAmL80PAf_qSrmLz19zH-n6Z5KHT76UpAc",
+  authDomain: "nextfirebase-aceb6.firebaseapp.com",
+  databaseURL: "https://nextfirebase-aceb6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nextfirebase-aceb6",
+  storageBucket: "nextfirebase-aceb6.appspot.com",
+  messagingSenderId: "1056292768304",
+  appId: "1:1056292768304:web:0ced01da8c8b5688533bf5",
+  measurementId: "G-1QDHYV6J28"
 };
 
 const app = initializeApp(firebaseConfig);  
---
+++ cypress/auth/src/components/Home.js
@@ -0,0 +1,83 @@
+'use client';
+import { db } from '@/app/config'; 
+import { collection, addDoc } from 'firebase/firestore';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'; 
+
+async function addDataToFireStore(name, password) {
+  try {
+    const docRef = await addDoc(collection(db, "message"), {
+      name: name,
+      password: password,
+    });
+    console.log("Document written:", docRef.id);
+    return true;
+  } catch (error) {
+    console.log("Error Add", error)
+    return false;
+  }
+}
+
+export default function Home() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const router = useRouter(); 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const added = await addDataToFireStore(name, password);
+    if (added) {
+      setName("");
+      setPassword("");
+      setSuccessMessage("Successfully!");
+      router.push('/new-page'); 
+    }
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <form 
+        onSubmit={handleSubmit}
+        className='max-w-md mx-auto p-4 bg-white shadow-md rounded-lg'
+      >
+        <div className='mb-4'>
+          <label htmlFor='name' className='block text-gray-700 font-bold mb-2'>
+            Name: 
+          </label>
+          <input
+            type='text'
+            id='name'
+            className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='password' className='block text-gray-700 font-bold mb-2'>
+            Password: 
+          </label>
+          <input
+            type='password'
+            id='password'
+            className='w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className='text-center'>
+          <button
+            type='submit'
+            data-testId='submit-button'
+            className='bg-blue-500 text-white py-2 px-2'
+          >
+            Login
+          </button>
+        </div>
+      </form>
+      <div className="mt-4 text-green-500" data-testid="success-message">
+        {successMessage}
+      </div>
+    </main>
+  );
+}
